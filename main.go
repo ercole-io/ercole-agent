f46972ca87142cc7d8ac52ffdf1412e17795ceac
amreo <amreo@live.it>
2020-02-05 16:34:31 +0100

    Added debug prints

--- main.go
+++ main.go
@@ -272,8 +272,6 @@ func fetcher(configuration config.Configuration, fetcherName string, params ...s
 		stderr bytes.Buffer
 	)
 
-	log.Println("Fetching", fetcherName)
-
 	baseDir := config.GetBaseDir()
 
 	if runtime.GOOS == "windows" {
@@ -286,8 +284,11 @@ func fetcher(configuration config.Configuration, fetcherName string, params ...s
 		} else {
 			params = append([]string{"-version", configuration.ForcePwshVersion, "-ExecutionPolicy", "Bypass", "-File", baseDir + "\\fetch\\win.ps1", "-s", fetcherName}, params...)
 		}
+		log.Println("Fetching " + psexe + " " + strings.Join(params, " "))
+
 		cmd = exec.Command(psexe, params...)
 	} else {
+		log.Println("Fetching " + baseDir + "/fetch/" + fetcherName + " " + strings.Join(params, " "))
 		cmd = exec.Command(baseDir+"/fetch/"+fetcherName, params...)
 	}
 
