533191ed30ea67371441c4ec6e71aeafd61815b5
Fabrizio Cirelli <cirelli94@gmail.com>
2020-06-22 17:58:18 +0200

    Updated host.sh

--- fetch/linux/host.sh
+++ fetch/linux/host.sh
@@ -15,35 +15,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-OPERATING_SYSTEM=LINUX
-LICENCING_CPU_CORES=$(grep processor /proc/cpuinfo | wc -l)
 HOSTNAME=$(hostname)
+
 CPU_MODEL=$(grep "model name" /proc/cpuinfo | sort -u | awk -F ': ' '{print $2}')
-CPU_THREAD=$(grep processor /proc/cpuinfo | wc -l)
-if [[ $CPU_THREAD = '1' ]]; then
+
+if [[ $(echo $CPU_MODEL | grep -c "@") ]]; then
+  CPU_FREQUENCY=$(echo $CPU_MODEL | awk -F '@' '{print $2}')
+else
+  CPU_FREQUENCY=$(grep "cpu MHz" /proc/cpuinfo | head -n 1 | awk -F ":" '{ print $NF }')Mhz
+fi
+
+CPU_MODEL=$(echo $CPU_MODEL | awk -F '@' '{print $1}')
+
+CPU_THREADS=$(grep processor /proc/cpuinfo | wc -l)
+if [[ $CPU_THREADS = '1' ]]; then
   CPU_CORES=1
-else 
-  CPU_CORES=$(($CPU_THREAD / 2))
+else
+  CPU_CORES=$(($CPU_THREADS / 2))
 fi
+
+THREADS_PER_CORE=$(expr $CPU_THREADS / $CPU_CORES)
+
 if [[ $VIRTUAL = 'Y' ]]; then
-  SOCKET=1
+  CPU_SOCKETS=1
 else
-  SOCKET=$(cat /proc/cpuinfo |grep -i "physical id" |sort -n|uniq|wc -l)
-fi 
-
-if [[ $(echo $CPU_MODEL| grep -c "@") -eq 0 ]]; then
-	CPU_EXTRA_SPEED=@$(grep "cpu MHz" /proc/cpuinfo | head -n 1 | awk -F ":" '{ print $NF }')Mhz
-else
-	CPU_EXTRA_SPEED=
+  CPU_SOCKETS=$(cat /proc/cpuinfo | grep -i "physical id" | sort -n | uniq | wc -l)
 fi
 
-KERNEL=$(uname -r)
+CORES_PER_SOCKET=$(expr $CPU_CORES / $CPU_SOCKETS)
+
+KERNEL=$(uname --kernel-name)
+KERNEL_VERSION=$(uname --kernel-release)
+
 OS=$(cat /etc/redhat-release)
 if [[ $? != 0 ]]; then
-  OS=$(cat /etc/SuSE-release| head -1)
+  OS=$(cat /etc/SuSE-release | head -1)
 fi
-MEM_TOTAL=$(echo "$(($(free -k | grep Mem | awk -F ' ' '{print $2}')/1024/1024))")
-SWP_TOTAL=$(echo "$(($(free -k | grep Swap | awk -F ' ' '{print $2}')/1024/1024))")
+
+OS=$(cat /etc/os-release | grep "^NAME=" | awk -F\= '{gsub(/"/,"",$2);print $2}')
+OS_VERSION=$(cat /etc/os-release | grep "^VERSION_ID=" | awk -F\= '{gsub(/"/,"",$2);print $2}')
+
+MEM_TOTAL=$(echo "$(($(free -k | grep Mem | awk -F ' ' '{print $2}') / 1024 / 1024))")
+SWP_TOTAL=$(echo "$(($(free -k | grep Swap | awk -F ' ' '{print $2}') / 1024 / 1024))")
 
 CHECK_SUN_CLUSTER=$(ps -eo cmd | grep -v grep | grep "/rpc.pmfd\b" | wc -l)
 if [ $CHECK_SUN_CLUSTER = 1 ]; then
@@ -66,47 +79,55 @@ else
   ORACLE_CLUSTER=N
 fi
 
-
 CHECK_TYPE_SERVER_OVM_DMESG=$(dmesg | grep OVM | wc -l)
 CHECK_TYPE_SERVER_OVM_LOG=$(grep OVM /var/log/dmesg | wc -l)
 CHECK_TYPE_SERVER_VMWARE=$(dmesg | grep VMware | wc -l)
 CHECK_TYPE_SERVER_VMWARE_LOG=$(grep VMware /var/log/dmesg* | wc -l)
-CHECK_TYPE_SERVER_HYPERV=$(dmesg | grep HyperV|wc -l)
+CHECK_TYPE_SERVER_HYPERV=$(dmesg | grep HyperV | wc -l)
 CHECK_TYPE_SERVER_HYPERV_LOG=$(grep HyperV /var/log/dmesg* | wc -l)
-# CHECK_TYPE_SERVER_AIX=$(uname -L | awk '{print $1}')
-# TODO: HPUX has a specific server type, see original script
 CHECK_TYPE_SERVER_HPUX=0
 CHECK_TYPE_SERVER_HYPERVISOR=$(grep ^flags /proc/cpuinfo | grep hypervisor | wc -l)
 
 if [ "$CHECK_TYPE_SERVER_OVM_DMESG" -gt 0 ] || [ "$CHECK_TYPE_SERVER_OVM_LOG" -gt 0 ]; then
-  TYPE=OVM
+  HARDWARE_ABSTRACTION_TECHNOLOGY=OVM
   VIRTUAL=Y
 elif [ $CHECK_TYPE_SERVER_VMWARE -gt 0 ] || [ "$CHECK_TYPE_SERVER_VMWARE_LOG" -gt 0 ]; then
-  TYPE=VMWARE
+  HARDWARE_ABSTRACTION_TECHNOLOGY=VMWARE
   VIRTUAL=Y
 elif [ $CHECK_TYPE_SERVER_HYPERV -gt 0 ] || [ "$CHECK_TYPE_SERVER_HYPERV_LOG" -gt 0 ]; then
-  TYPE=HYPERV
+  HARDWARE_ABSTRACTION_TECHNOLOGY=HYPERV
   VIRTUAL=Y
 elif [ $CHECK_TYPE_SERVER_HYPERVISOR -gt 0 ]; then
-  TYPE=VMOTHER
+  HARDWARE_ABSTRACTION_TECHNOLOGY=VMOTHER
   VIRTUAL=Y
 else
-  TYPE=PH
+  HARDWARE_ABSTRACTION_TECHNOLOGY=PH
   VIRTUAL=N
 fi
 
-echo -n "hostname: $HOSTNAME
-cpumodel: $CPU_MODEL $CPU_EXTRA_SPEED
-cputhreads: $CPU_THREAD
-cpucores: $CPU_CORES
-socket: $SOCKET
-type: $TYPE
-virtual: $VIRTUAL
-kernel: $KERNEL
-os: $OS
-memorytotal: $MEM_TOTAL
-swaptotal: $SWP_TOTAL
-oraclecluster: $ORACLE_CLUSTER
-veritascluster: $VERITAS_CLUSTER
-suncluster: $SUN_CLUSTER
-aixcluster: N" 
+if [ $VIRTUAL == "Y" ]; then
+  HARDWARE_ABSTRACTION="VIRT"
+else
+  HARDWARE_ABSTRACTION="PH"
+fi
+
+echo -n "Hostname: $HOSTNAME
+CPUModel: $CPU_MODEL 
+CPUFrequency: $CPU_FREQUENCY
+CPUSockets: $CPU_SOCKETS
+CPUCores: $CPU_CORES
+CPUThreads: $CPU_THREADS
+ThreadsPerCore: $THREADS_PER_CORE
+CoresPerSocket: $CORES_PER_SOCKET 
+HardwareAbstraction: $HARDWARE_ABSTRACTION
+HardwareAbstractionTechnology: $HARDWARE_ABSTRACTION_TECHNOLOGY
+Kernel: $KERNEL
+KernelVersion: $KERNEL_VERSION
+OS: $OS
+OSVersion: $OS_VERSION
+MemoryTotal: $MEM_TOTAL
+SwapTotal: $SWP_TOTAL
+OracleCluster: $ORACLE_CLUSTER
+VeritasCluster: $VERITAS_CLUSTER
+SunCluster: $SUN_CLUSTER
+AixCluster: N"
