1e03c749fe9b4a2d3af94056c66b6a75d99d5083
Fabrizio Cirelli <cirelli94@gmail.com>
2021-11-16 14:55:09 +0100

    Fix missing FACTOR value

--- sql/license.sql
+++ sql/license.sql
@@ -13,6 +13,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+whenever sqlerror exit sql.sqlcode
+
 set feedback off pages 0 serverout on verify off lines 123 timing off
 -- Prepare settings for pre 12c databases
 define DFUS=DBA_
