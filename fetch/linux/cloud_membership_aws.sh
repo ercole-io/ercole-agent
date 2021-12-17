8d34d0790919026e5ba304cdde1b1dfe008bf008
Fabrizio Cirelli <cirelli94@gmail.com>
2021-12-17 10:31:49 +0100

    Improved fetcher script

--- fetch/linux/cloud_membership_aws.sh
+++ fetch/linux/cloud_membership_aws.sh
@@ -15,10 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-AWS=$(grep -i amazon --count /sys/devices/virtual/dmi/id/bios_version )
+# https://serverfault.com/questions/462903/how-to-know-if-a-machine-is-an-ec2-instance
 
-if [ "$AWS" -gt 0 ]; then
-    echo "true"
+# This first, simple check will work for many older instance types.
+if [ -f /sys/hypervisor/uuid ]; then
+  # File should be readable by non-root users.
+  if [ "$(head -c 3 /sys/hypervisor/uuid)" = "ec2" ]; then
+      echo "true"
+  else
+      echo "false"
+  fi
+
+# This check will work on newer m5/c5 instances, but only if you have root!
+elif [ -r /sys/devices/virtual/dmi/id/product_uuid ]; then
+  # If the file exists AND is readable by us, we can rely on it.
+  if [ "$(head -c 3 /sys/devices/virtual/dmi/id/product_uuid)" = "EC2" ]; then
+      echo "true"
+  else
+      echo "false"
+  fi
+
 else
-    echo "false"
+  # Fallback check of http://169.254.169.254/. If we wanted to be REALLY
+  # authoritative, we could follow Amazon's suggestions for cryptographically
+  # verifying their signature, see here:
+  #    https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+  # but this is almost certainly overkill for this purpose (and the above
+  # checks of "EC2" prefixes have a higher false positive potential, anyway).
+  if "$(curl -s -m 5 http://169.254.169.254/latest/dynamic/instance-identity/document | grep -q availabilityZone)" ; then
+      echo "true"
+  else
+      echo "false"
+  fi
+
 fi
