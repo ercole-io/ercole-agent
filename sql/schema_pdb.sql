2e914f50992aea9752eead86d476969a503c7db8
Raffaello Ferrari <47225245+raffaelloferrari@users.noreply.github.com>
2022-11-29 14:49:42 +0100

    Add field account status

--- sql/schema_pdb.sql
+++ sql/schema_pdb.sql
@@ -27,25 +27,13 @@ alter session set container=&1;
 
 with 
 lista_users as
-(select username from dba_users where username not in ('SYS','AUDSYS','SYSTEM','SYSBACKUP','SYSDG','SYSKM','OUTLN','GSMADMIN_INTERNAL',
+(select username,account_status from dba_users where username not in ('SYS','AUDSYS','SYSTEM','SYSBACKUP','SYSDG','SYSKM','OUTLN','GSMADMIN_INTERNAL',
 'GSMUSER','DIP','XS$NULL','ORACLE_OCM','DBSNMP','APPQOSSYS','ANONYMOUS','XDB',
 'GSMCATUSER','WMSYS','OJVMSYS','CTXSYS','ORDDATA','ORDSYS','ORDPLUGINS',
 'SI_INFORMTN_SCHEMA','MDSYS','OLAPSYS','MDDATA','SPATIAL_WFS_ADMIN_USR',
 'SPATIAL_CSW_ADMIN_USR','LBACSYS','APEX_040200','APEX_PUBLIC_USER','FLOWS_FILES',
 'DVSYS','DVF','SCOTT','EXFSYS','XS\$NULL','CMDB_USR','CPFI0_APPPRD','SYSMAN','SYSADMIN',
 'MGMT_VIEW','DMSYS','WCCREPUSER','WCCUSER','WFADMIN','OWF_MGR','KWALKER','BLEWIS','CDOUGLAS','SPIERSON')
-and username not like 'APEX%'
-and username not like 'RMAN%'
-and username not like 'U0%'
-and username not like 'PATROL%'
-and username not like 'OWB%'
-and username not like '%WRPRD'
-and username not like '%ROPRD'
-and username not like '%GESTIONE'
-and username not like 'FLOWS%'
-and username not like 'SQLTX%'
-and username not like 'DBA_%'
---and AUTHENTICATION_TYPE not like '%EXTERNAL%'
 ),
 tbmb as
 (select owner,round(sum(bytes/1024/1024)) as "A" from dba_segments where segment_type like 'TABLE%' group by owner),
@@ -61,13 +49,14 @@ select
 	   nvl(round(sum(s.bytes/1024/1024)),0) as "TOTMB",
 	   nvl(t.A,0) as "TBMB",
 	   nvl(i.B,0) as "INDMB",
-	   nvl(l.C,0) as "LOBMB"
+	   nvl(l.C,0) as "LOBMB",
+	   u.account_status
 	   from 
 	   lista_users u 
 	   left join dba_segments s on  u.username=s.owner
 	   left join tbmb t on  u.username=t.owner
 	   left join indmb i on  u.username=i.owner
 	   left join lobmb l on  u.username=l.owner
-	   group by u.username,t.a,i.b,l.c order by 1;
+	   group by u.username,u.account_status,t.a,i.b,l.c order by 1;
 
 exit
