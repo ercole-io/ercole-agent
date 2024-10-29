1dc118872db0b19cf428fe52a3eb608e85a7b64b
afumagalli98 <a.fuma.98@gmail.com>
2024-10-29 14:12:58 +0100

    Updated db & size pdb sql

--- sql/size_pdb.sql
+++ sql/size_pdb.sql
@@ -34,7 +34,7 @@ SELECT
     (
         (SELECT round(sum(decode(autoextensible,'NO',bytes/1024/1024/1024,'YES',maxbytes/1024/1024/1024)))
         FROM dba_data_files)+
-        (SELECT round(sum(bytes/1024/1024/1024))
+        (SELECT round(sum(decode(autoextensible,'NO',bytes/1024/1024/1024,'YES',maxbytes/1024/1024/1024)))
         FROM dba_temp_files)+
         (SELECT round(sum(bytes/1024/1024/1024))
         FROM v$log)
